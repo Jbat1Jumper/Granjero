e4aface27db72d0b0ecc338b758d879909b223af
Jbat1Jumper <nikitaz@infobiz.com.ar>
2014-08-05 23:18:16 -0300

    agregada escena de prueba con un controlador y se limpio un poco

---
+++ Assets/img/fasttest/GranjeroTest.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GranjeroTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	public float Speed = 1f;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.D))
+			this.transform.position += new Vector3(Speed, 0, 0);
+		if(Input.GetKey(KeyCode.A))
+			this.transform.position += new Vector3(-Speed, 0, 0);
+		if(Input.GetKey(KeyCode.S))
+			this.transform.position += new Vector3(0, -Speed, 0);
+		if(Input.GetKey(KeyCode.W))
+			this.transform.position += new Vector3(0, Speed, 0);
+	}
+}
