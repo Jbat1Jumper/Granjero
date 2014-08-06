90de931bbe041c8aabc79de15ccba387e7fe9121
Jbat1Jumper <nikitaz@infobiz.com.ar>
2014-08-06 00:17:15 -0300

    cereza asesina

---
+++ Assets/img/fasttest/CherryTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CherryTest : MonoBehaviour {
+
+	public float Speed = 0.05f;
+	public float Distance = 4f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var player = GetPlayer();
+		if(player != null) {
+			ChasePlayer(player);
+		}
+	}
+
+	GameObject GetPlayer() {
+		var p = new Vector2(transform.position.x, transform.position.y);
+		var l = Physics2D.OverlapCircleAll(p, Distance);
+		foreach(var collider in l){
+			var go = collider.gameObject;
+			if(go.CompareTag("player"))
+				return go;
+		}
+		return null;
+	}
+
+	void ChasePlayer(GameObject player) {
+		var d = (player.transform.position - this.transform.position);
+		d.z = 0f;
+		d.Normalize();
+		this.transform.position += (d * this.Speed);
+	}
+}
